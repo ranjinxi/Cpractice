1cc61c37c85639460ce70cc727d079f9b20dd607
ranjinxi <r1367718372@163.com>
2019-08-07 15:55:47 +0800

    19.8.7

---
+++ 14.1.elur.c
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: 14.elur.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年08月07日 星期三 14时49分23秒
+ ************************************************************************/
+
+#include<stdio.h>
+#define max_n 1000000
+#define keep_max_len 1000000
+
+int keep[keep_max_len] = {0};
+
+long long get_len(long long  x) {
+    if (x == 1) return 1;
+    if (x < keep_max_len && keep[x]) return keep[x];
+    int ret;
+    if (x & 1) ret = get_len(x * 3 +1) +1;
+    else ret =  get_len(x >> 1) +1;
+    if (x < keep_max_len) keep[x] = ret;
+    return ret;
+}
+
+int main() {
+    int ans = 0, num = 0;
+    int l;
+    for (int i = 1; i < max_n; i++) {
+        l = get_len(i);
+        if (l > ans) ans = l, num = i;
+    }
+    printf("%d\n", num);
+    return 0;
+}
