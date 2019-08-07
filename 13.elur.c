3decc8c4461b0236d111a17caa3329d9b8696e70
ranjinxi <r1367718372@163.com>
2019-08-07 18:48:09 +0800

    "19.8.7"

---
+++ 13.elur.c
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: 13.elur.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年08月07日 星期三 18时08分31秒
+ ************************************************************************/
+#include<stdio.h>
+#include <string.h>
+
+char num[55];
+int ans[55] = {0};
+
+int main() {
+    for(int i = 0; i < 100; i++) {
+        scanf("%s", num);
+        int len = strlen(num);
+        if (ans[0] < len) ans[0] = len;
+        for (int i = 0; i < len; i++) {
+            ans[len -i] += (num[i] - '0');
+        }
+        for (int i = 1; i <= ans[0]; i++) {
+            if (ans[i] < 10) continue;
+            ans[i + 1] += ans[i] / 10;
+            ans[i] %= 10;
+            ans[0] += (i == ans[0]);
+        }
+    }
+    for (int i = ans[0]; i > ans[0] - 10; i--) {
+        printf("%d", ans[i]);
+    }
+    printf("\n");
+    return 0;
+}
