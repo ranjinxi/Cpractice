05b0980cb5926fdbeafc196a0ea3b38ea7958c56
ranjinxi <r1367718372@163.com>
2019-08-07 13:13:05 +0800

    19.8.7

--- 8.elur.c
+++ 8.elur.c
@@ -9,7 +9,7 @@
 #include "ep08.h"
 
 int main() {
-   long long  p = 1, zero = 0, ans = 0;
+    long long p = 1, zero = 0, ans = 0;
     for (int i = 0; num[i]; i++) {
         if (num[i] == '0') {
             zero += 1;
@@ -17,13 +17,13 @@ int main() {
             p *= (num[i] - '0');
         }
         if (i >= 13) {
-            if (num[i - 13] == '0') {
+            if (num[i -13] == '0') {
                 zero -= 1;
             } else {
                 p /= (num[i -13] - '0');
             }
         }
-        if (zero == 0 && p > ans) ans =p;
+        if (zero == 0 && p > ans) ans = p;
     }
     printf("%lld", ans);
     return 0;
