301f7857da38fc133c7324969976744af5e40820
ranjinxi <r1367718372@163.com>
2019-09-03 09:54:55 +0800

    19. 9. 3

--- 33.elur.cpp
+++ 33.elur.cpp
@@ -4,30 +4,37 @@
 	> Mail: 
 	> Created Time: 2019年08月09日 星期五 16时18分16秒
  ************************************************************************/
-
-#include<iostream>
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 
-int gcd(int x, int y) {
-    return (y ? gcd(y, x % y) : x);
+int gcd(int a, int b) {
+    return (b ? gcd(b, a % b) : a);
 }
 
-int is_true(int s, int t) {
-    int ans = gcd(s, t);
-
+bool isNonTrival(int x, int y) {
+    if (x > 100 || x < 10) return false;
+    if (y > 100 || y < 10) return false;
+    int x1, x2, y1, y2;
+    x1 = x / 10, x2 = x % 10;
+    y1 = y / 10, y2 = y % 10;
+    if (!x1 || !x2 || !y1 || !y2) return false;
+    if (x1 == y1 && x2 * y == y2 * x && x2 != 0) return true;
+    if (x1 == y2 && x2 * y == y1 * x && x2 != 0) return true;
+    if (x2 == y2 && x1 * y == y1 * x && x1 != 0) return true;
+    if (x2 == y1 && x1 * y == y2 * x && x1 != 0) return true;
+    return false;
 }
 
 int main() {
-    int n = 1, m = 1;
-    for (int a = 11; a < 100; a++) {
-        for (int b = a + 1; b < 100; b++) {
-           if(!is_true(a, b)) continue;
-            n *= a;
-            n *= b;
+    int x = 1, y = 1, c;
+    for (int i = 10; i <= 100; i++) {
+        for (int j = i + 1; j <= 100; j++) {
+            if (!isNonTrival(i, j)) continue;
+            x *= i, y *= j;
+            c = gcd(x, y);
+            x /= c, y /= c;
         }
     }
-    printf("%d\n", m / gcd(n,m));
-     is_true(11, 22);
+    cout << y << endl;
     return 0;
 }
--- 35.elur.cpp
+++ 35.elur.cpp
@@ -4,22 +4,43 @@
 	> Mail: 
 	> Created Time: 2019年08月09日 星期五 17时59分36秒
  ************************************************************************/
+#include <iostream>
+#include <cmath>
+#define max_n 1000000
+using namespace std;
+
+int prime[max_n + 5] = {0};
+int is_prime[max_n + 5] = {0};
 
 void init() {
     for (int i = 2; i <= max_n; i++) {
-        if(!is_prime[i]) prime[++prime[0]] = i;
+        if (!is_prime[i]) prime[++prime[0]] = i;
         for (int j = 1; j <= prime[0]; j++) {
-            if (prime[j] * i > max_n) break;
-            is_prime[prime[j] * i] = 1;
-            if (prime[j] * i == 0) break;
+            if (i * prime[j] > max_n) break;
+            is_prime[i * prime[j]] = 1;
+            if (i % prime[j] == 0) break;
         }
     }
 }
 
-int main() {
-    init ();
-         int sum = 0;
-    for (int i = 1; i <= prime[0]; i++) {
-        :
+int is_cirle_prime(int n) {
+    int h, j;
+    h = (int)pow(10, floor(log10(n)));
+    j = floor(log10(n));
+    for (int i = 0; i < j; i++) {
+        n = n / 10 + (n % 10)  * h;
+        if (is_prime[n]) return 0;
     }
+    return 1;
+}
+
+
+int main() {
+    init();
+    int num = 0;
+    for (int i = 1; i <= prime[0]; i++) {
+        if (is_cirle_prime(prime[i])) num++;
+    }
+    cout << num << endl;
+    return 0;
 }
