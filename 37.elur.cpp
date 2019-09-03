c737ed3c52a57ed59be1529068b521a093e66db7
ranjinxi <r1367718372@163.com>
2019-09-03 10:54:01 +0800

    19.9.3

--- 37.elur.cpp
+++ 37.elur.cpp
@@ -4,50 +4,54 @@
 	> Mail: 
 	> Created Time: 2019年08月09日 星期五 18时41分49秒
  ************************************************************************/
-#include <stdio.h>
-#include<iostream>
-#include <math.h>
-#define max_n 1000000
+ #include <iostream>
+ #include <cmath>
+ #define max_n 1000000
 using namespace std;
-int prime[max_n + 5] = {0};
-int is_prime[max_n + 5] = {0};
+
+ int is_prime[max_n + 5] = {0};
+ int prime[max_n + 5] = {0};
+
 
 void init() {
-for (int i = 2; i <= max_n; i++) {
+    for (int i = 2; i <= max_n; i++) {
         if (!is_prime[i]) prime[++prime[0]] = i;
         for (int j = 1; j <= prime[0]; j++) {
             if (i * prime[j] > max_n) break;
-           is_prime[i * prime[j]] = 1;
+            is_prime[i * prime[j]] = 1;
             if (i % prime[j] == 0) break;
         }
     }
 }
 
-int is_val(int x) {
-    int l = floor(log10(x)) + 1;
-    int temp = x;
-    for (int i = 0; i < l -1; i++) {
-        int h = (int)pow(10, i);
-        temp %= h;
-        if(!is_prime[temp]) continue;
-        return 0;
+bool is_ca(int n) {
+    if (n <= 10) return false;
+    int x = n;
+    while (x) {
+        if (is_prime[x]) return false;
+        x = x / 10;
     }
-    for (int i = 0; i  < l -1; i++) {
-       int h = (int)pow(10,i);
-        temp /= h;
-        if(!is_prime[temp]) continue;
-        return 0;
+    int j = floor(log10(n)) + 1;
+    for (int i = 0; i < j; i++) {
+        if (is_prime[n]) return false;
+        n = n  % (int)pow(10, j - i - 1);
     }
-    return 1;
+    return true;
 }
 
-int main() {
+ int main() {
+    init();
+    is_prime[1] = 1;
     int sum = 0;
-    for (int i = 4; i <= prime[0]; i++) {
-        if (!is_val(prime[i])) continue;
-        else  sum += prime[i];
+    int num = 0;
+    for (int i = 1; i <= prime[0]; i++) {
+        if (is_ca(prime[i]))
+        {  cout << prime[i] << endl;  
+        sum += prime[i], num++;
+        }
     }
-    printf("%d\n", sum);
-    printf ("%d\n",is_val(29));
+     cout << sum << endl;
+     cout << num << endl;
+     cout << is_prime[1] << endl;
     return 0;
  }
